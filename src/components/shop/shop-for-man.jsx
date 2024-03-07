00f4bc831cd20f121a44c9ed3bf7255e50ccb4f2
Alesha <121282755+oAleshao@users.noreply.github.com>
2024-03-07 18:29:55 +0300

    I've added sort

--- src/components/shop/shop-for-man.jsx
+++ src/components/shop/shop-for-man.jsx
@@ -4,6 +4,8 @@ import ProductList from './product-list';
 import products from './products'
 import ShowProduct from './show-product';
 import ProductAdd from './product-add';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { nanoid } from 'nanoid';
 
 
@@ -15,6 +17,8 @@ const ShopForMan = () => {
     const [product, setProduct] = useState('');
     const [formVis, setformVis] = useState(false);
     const [vanishFav, setVanishfav] = useState(false);
+    const [typeSort, setTypeSort] = useState('');
+    const [ascOrDesc, setAscOrDesc] = useState('asc');
 
     const openForm = () => {
         setformVis(!formVis);
@@ -59,22 +63,82 @@ const ShopForMan = () => {
         setVanishfav(!vanishFav);
     };
 
+    const sortList = (type) => {
+        if (type === 'byName') {
+            setTypeSort(type);
+            setlist(sortByTitle(list));
+            setFavlist(sortByTitle(favlist));
+        }
+        else if (type === 'byPrice') {
+            setTypeSort(type);
+            setlist(sortByPrice(list));
+            setFavlist(sortByPrice(favlist));
+        }
+    };
+
+    const sortByTitle = (collection) => {
+        if (ascOrDesc === 'asc') {
+            setAscOrDesc('desc');
+            return [...collection.sort((first, second) => {
+                return first.title.charCodeAt(0) - second.title.charCodeAt(0)
+            })]
+        }
+        else {
+            setAscOrDesc('asc');
+            return [...collection.sort((first, second) => {
+                return second.title.charCodeAt(0) - first.title.charCodeAt(0)
+            })]
+        }
+    };
+
+    const sortByPrice = (collection) => {
+        if (ascOrDesc === 'asc') {
+            setAscOrDesc('desc');
+            return [...collection.sort((first, second) => {
+                return first.price - second.price;
+            })];
+        }
+        else {
+            setAscOrDesc('asc');
+            return [...collection.sort((first, second) => {
+                return second.price - first.price;
+            })];
+        }
+    };
+
 
     return (
         <div id='MensShop'>
             <ShopLogo openForm={openForm} favlist={favlist} openFavList={openFavList} />
 
             <div id="offersBody">
-                <h1 id='offers'>
-                    {vanishFav ?
-                        'Favorites' :
-                        'Special offers'
-                    }
-                </h1>
+                <div id='boxOf'>
+                    <h1 id='offers'>
+                        {vanishFav ?
+                            'Favorites' :
+                            'Special offers'
+                        }
+                    </h1>
+                    <div id='sortBox'>
+                        <p>Sort by: </p>
+                        <div className='styleSort' onClick={() => { sortList('byName') }}>
+                            <h1>A-Z</h1>
+                            {typeSort === 'byName' ?
+                                ascOrDesc === 'asc' ? <KeyboardArrowUpIcon className='iconSort' /> : <KeyboardArrowDownIcon className='iconSort' /> : ''
+                            }
+                        </div>
+                        <div className='styleSort' onClick={() => { sortList('byPrice') }}>
+                            <h1>Price</h1>
+                            {typeSort === 'byPrice' ?
+                                ascOrDesc === 'asc' ? <KeyboardArrowUpIcon className='iconSort' /> : <KeyboardArrowDownIcon className='iconSort' /> : ''
+                            }
+                        </div>
+                    </div>
+                </div>
                 <div id='shopBodyList'>
                     {vanishFav ?
                         favlist.map((product) => (<ProductList {...product} product={product} ChooseProduct={ChooseProduct} changeFavlist={changeFavlist} key={product.id} />)) :
-                        list.map((product) => (<ProductList {...product} product={product} ChooseProduct={ChooseProduct} changeFavlist={changeFavlist} key={product.id} />))
+                        list?.map((product) => (<ProductList {...product} product={product} ChooseProduct={ChooseProduct} changeFavlist={changeFavlist} key={product.id} />))
                     }
                 </div>
             </div>
